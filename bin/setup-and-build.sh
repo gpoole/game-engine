46c67136e673c8796ba413082f98e53e5360751b
Greg Poole <greg.poole@protonmail.com>
2024-03-02 22:39:47 +1100

    Tidy up spacing

--- bin/setup-and-build.sh
+++ bin/setup-and-build.sh
@@ -3,5 +3,5 @@
 rm -rf build
 conan install . --output-folder=build --build=missing
 cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release 
-cmake --build . 
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+cmake --build .
